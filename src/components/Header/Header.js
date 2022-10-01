6546b3129e1f79b401ace65ced391c4b80f641ee
Igor <f.igormanoel123@gmail.com>
2022-10-01 16:48:33 -0300

    updates

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,22 +14,24 @@ function Header (){
     return(
         <div className={styles.container}>
             <header className={styles.header}>
-                <div className={styles.logo} onClick={menuOpen}>
-                    <Link to="/" spy={true} smooth={true} offset={0} duration={700}></Link>
-                </div>
+                
+                    <Link to="/" spy={true} smooth={true} offset={0} duration={700}>
+                        <div className={styles.logo} onClick={menuOpen}> </div> 
+                    </Link>
+                
                     
                     {menu === true &&
                         <MenuMobile/>
                     }
                 <ul className={styles.menu}>
                     <li>
-                        <Link to="sobremim" spy={true} smooth={true} offset={0} duration={700} >Sobre mim</Link>
+                        <Link to="sobremim" spy={true} smooth={true} offset={-10} duration={700} >Sobre mim</Link>
                     </li>
                     <li>
-                        <Link to="projects" spy={true} smooth={true} offset={0} duration={700} >Meus Projetos</Link>
+                        <Link to="projects" spy={true} smooth={true} offset={-75} duration={700} >Meus Projetos</Link>
                     </li>
                     <li>
-                        <Link to="skills" spy={true} smooth={true} offset={0} duration={700} >Minhas Skills</Link>
+                        <Link to="skills" spy={true} smooth={true} offset={-75} duration={700} >Minhas Skills</Link>
                     </li>
                     <li>
                         <Link to="footer" spy={true} smooth={true} offset={0} duration={700} >Contato</Link>
